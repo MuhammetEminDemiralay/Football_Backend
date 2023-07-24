cbf5c173fcfccbd89eeaab5c20e6365f4c595c6c
muhammet demiralay <pepmuhammet.42@gmail.com>
2023-07-24 14:40:07 +0300

    EntityFramework and Repository added

---
+++ DataAccess/Concrete/EntityFramework/FootballContext.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class FootballContext : DbContext
+    {
+        public DbSet<CareerStat> CareerStats { get; set; }
+        public DbSet<City> Citys { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Continent> Continents { get; set; }
+        public DbSet<Country> Countrys { get; set; }
+        public DbSet<Footballer> Footballers { get; set; }
+        public DbSet<League> Leagues { get; set; }
+        public DbSet<NationalTeam> NationalTeams { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<TransferHistory> TransferHistorys { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\\MSSQLLocalDB;Database=Football;");
+        }
+
+
+    }
+}
