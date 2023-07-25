c8182a2a1fc0c6097aa6bf8801fa959e60c67e61
muhammet demiralay <pepmuhammet.42@gmail.com>
2023-07-25 18:57:44 +0300

    Dependencies resolved with Ninject.

---
+++ Business/DependencyResolvers/Ninject/NinjectBusinessModule.cs
@@ -0,0 +1,52 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Ninject
+{
+    public class NinjectBusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ICareerStatService>().To<CareerStatManager>().InSingletonScope();
+            Bind<ICareerStatDal>().To<EFCareerStatDal>().InSingletonScope();
+
+            Bind<ICityService>().To<CityManager>().InSingletonScope();
+            Bind<ICityDal>().To<EFCityDal>().InTransientScope();
+
+            Bind<IClubService>().To<ClubManager>().InSingletonScope();
+            Bind<IClubDal>().To<EFClubDal>().InSingletonScope();
+
+            Bind<IContinentService>().To<ContinentManager>().InSingletonScope();
+            Bind<IContinentDal>().To<EFContinentDal>().InSingletonScope();
+
+            Bind<ICountryService>().To<CountryManager>().InSingletonScope();
+            Bind<ICountryDal>().To<EFCountryDal>().InSingletonScope();
+
+            Bind<IFootballerService>().To<FootballerManager>().InSingletonScope();
+            Bind<IFootballerDal>().To<EFFootballerDal>().InSingletonScope();
+
+            Bind<ILeagueService>().To<LeagueManager>().InSingletonScope();
+            Bind<ILeagueDal>().To<EFLeagueDal>().InSingletonScope();
+
+            Bind<INationalTeamService>().To<NationalTeamManager>().InSingletonScope();
+            Bind<INationalTeamDal>().To<EFNationalTeamDal>().InSingletonScope();
+
+            Bind<IPositionService>().To<PositionManager>().InSingletonScope();
+            Bind<IPositionDal>().To<EFPositionDal>().InSingletonScope();
+
+            Bind<ITransferHistoryService>().To<TransferHistoryManager>().InSingletonScope();
+            Bind<ITransferHistoryDal>().To<EFTransferHistoryDal>().InSingletonScope();
+
+
+
+        }
+    }
+}
